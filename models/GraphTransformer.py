5dd98d7d6b03c02bb5c84402f334cc8f92ffd06d
peterlipan <847041172@qq.com>
2025-01-07 12:47:54 +0800

    GraphTransformer for surv

--- models/GraphTransformer.py
+++ models/GraphTransformer.py
@@ -9,8 +9,8 @@ from .utils import ModelOutputs
 from torch_geometric.nn import dense_mincut_pool
 
 
-class GraphTransformer(nn.Module):
-    def __init__(self, d_in, n_classes):
+class GraphTransformer(nn.Module): # TODO: Implement multi-task learning
+    def __init__(self, d_in, n_classes, surv_classes=4, task='cls'):
         super().__init__()
 
         self.embed_dim = 64
@@ -25,7 +25,7 @@ class GraphTransformer(nn.Module):
         self.conv1 = GCNBlock(d_in, self.embed_dim)  # 64->128
         self.pool1 = nn.Linear(self.embed_dim, self.node_cluster_num)  # 100-> 20
 
-    def forward(self, data):
+    def cls_forward(self, data):
 
         x, adj = data['x'].float(), data['adj'].float()
         bs = x.size(0)
@@ -43,3 +43,29 @@ class GraphTransformer(nn.Module):
         y_prob = F.softmax(logits, dim=1)
 
         return ModelOutputs(features=x, logits=logits, y_hat=y_hat, y_prob=y_prob)
+    
+    def surv_forward(self, data):
+        x, adj = data['x'].float(), data['adj'].float()
+        bs = x.size(0)
+
+        x = self.conv1(x, adj)
+        s = self.pool1(x)
+
+        x, adj, *_ = dense_mincut_pool(x, adj, s)
+        cls_token = self.cls_token.repeat(bs, 1, 1)
+        x = torch.cat([cls_token, x], dim=1)
+
+        logits, _ = self.transformer(x)
+
+        y_hat = torch.argmax(logits, dim=1)
+        hazards = torch.sigmoid(logits)
+        surv = torch.cumprod(1 - hazards, dim=1)
+        return ModelOutputs(features=x, logits=logits, y_hat=y_hat, hazards=hazards, surv=surv)
+    
+    def forward(self, data):
+        if 'cls' in self.task:
+            return self.cls_forward(data)
+        elif 'surv' in self.task:
+            return self.surv_forward(data)
+        else:
+            raise NotImplementedError
